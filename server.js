97d63f8d2a1cc194c0c3416b20a40bc037f9b099
Gershon Papiashvili <papigers93@gmail.com>
2016-05-12 20:59:57 +0300

    finish

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
+  var ip = req.connection.remoteAddress;
+  var language =  req.headers["accept-language"];
+  if(language.indexOf(',') > 0)
+    language = language.substring(0, language.indexOf(','));
+  var ua = req.headers["user-agent"];
+  var os = ua.substring(ua.indexOf('(') + 1, ua.indexOf(')'));
   
+  res.json({
+    'ipaddress': ip,
+    'language': language,
+    'software': os
+  });
 });
 
-app.set('port', process.env.PORT || 3000;);
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function(){
   console.log("Server listening on port: ", app.get('port'));
 });
